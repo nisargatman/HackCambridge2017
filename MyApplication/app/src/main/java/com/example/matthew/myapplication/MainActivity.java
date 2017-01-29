cddd15a7c0c4268b03753ecec012221b4cf73921
mattmarch <matthewmarch29@gmail.com>
2017-01-29 00:44:31 +0000

    lots and lots

--- MyApplication/app/src/main/java/com/example/matthew/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/matthew/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.matthew.myapplication;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +12,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import static com.example.matthew.myapplication.R.id.toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,23 +29,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        updateTable();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean updateTable() {
+        // Update items in table
+        TableLayout table = (TableLayout) findViewById(R.id.reciepts_table);
+        // Remove previous items
+        table.removeAllViews();
+
+        for (int i=0; i<30; i++) {
+            TableRow row = new TableRow(this);
+            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+            row.setLayoutParams(lp);
+            TextView vendor = new TextView(this);
+            vendor.setText("Cambridge Wine Merchants");
+            TextView cost = new TextView(this);
+            cost.setText("£300");
+            row.addView(vendor);
+            row.addView(cost);
+            table.addView(row);
+        }
         return true;
     }
 
@@ -47,5 +73,51 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private static final int SELECT_PICTURE = 1;
+
+    public void startCamera(View v) {
+
+//        Snackbar.make(v," Button pressed", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+//        Intent intent = new Intent(this, CameraActivity.class);
+//        startActivity(intent);
+
+
+        Intent pickIntent = new Intent();
+        pickIntent.setType("image/*");
+        pickIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        String pickTitle = "Select or take a new Picture"; // Or get from strings.xml
+        Intent chooserIntent = Intent.createChooser(pickIntent, pickTitle);
+        chooserIntent.putExtra
+                (
+                        Intent.EXTRA_INITIAL_INTENTS,
+                        new Intent[] { takePhotoIntent }
+                );
+
+        startActivityForResult(chooserIntent, SELECT_PICTURE);
+
+        return;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SELECT_PICTURE && resultCode == Activity.RESULT_OK) {
+            if (data == null) {
+                //Display an error
+                return;
+            }
+            try {
+                InputStream inputStream = this.getContentResolver().openInputStream(data.getData());
+                Snackbar.make(findViewById(toolbar),"Picture loaded", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            //Now you can do whatever you want with your inpustream, save it as file, upload to a server, decode a bitmap...
+        }
     }
 }
